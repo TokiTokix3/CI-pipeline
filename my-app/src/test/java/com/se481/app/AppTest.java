8e5e2ba45e0d273df8bff5d6d90914bc24da1e8e
Toki <36805172+TokiTokix3@users.noreply.github.com>
2020-12-12 15:21:05 -0800

    Initial

---
+++ my-app/src/test/java/com/se481/app/AppTest.java
@@ -0,0 +1,15 @@
+package com.se481.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+/**
+ * Unit test
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( false );
+    }
+}
